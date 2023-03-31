e03e5c82eac4fcb37b53d038fcb2177ad13bbf2e
Pin-yen Lin <treapking@chromium.org>
2023-03-31 17:11:40 +0800

    drm/bridge: anx7625: Check for Type-C during panel registration
    The output port endpoints can be connected to USB-C connectors.
    Running drm_of_find_panel_or_bridge() with such endpoints leads to
    a continuous return value of -EPROBE_DEFER, even though there is
    no panel present.
    
    To avoid this, check for the existence of a "mode-switch" property in
    the port endpoint, and skip panel registration completely if so.
    
    Signed-off-by: Pin-yen Lin <treapking@chromium.org>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Link: https://lore.kernel.org/r/20230331091145.737305-6-treapking@chromium.org
    Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>

--- drivers/gpu/drm/bridge/analogix/anx7625.c
+++ drivers/gpu/drm/bridge/analogix/anx7625.c
@@ -1889,7 +1889,17 @@ static int anx7625_parse_dt(struct device *dev,
 static int anx7625_parse_dt_panel(struct device *dev,
 				  struct anx7625_platform_data *pdata)
 {
-	struct device_node *np = dev->of_node;
+	struct device_node *np = dev->of_node, *port_node;
+	unsigned int count;
+
+	/*
+	 * Don't bother finding a panel if a Type-C `mode-switch` property is
+	 * present in one of the endpoints in the output port.
+	 */
+	port_node = of_graph_get_port_by_id(np, 1);
+	count = typec_mode_switch_node_count(&port_node->fwnode);
+	if (count)
+		return 0;
 
 	pdata->panel_bridge = devm_drm_of_get_bridge(dev, np, 1, 0);
 	if (IS_ERR(pdata->panel_bridge)) {
